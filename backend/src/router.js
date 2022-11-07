80efe5e9f2f3edcdf736a7384b1cc3a3c7bfd58a
raph2ll <raphael.ph2ll@gmail.com>
2022-11-07 11:03:27 -0300

    feat(router): new path

--- backend/src/router.js
+++ backend/src/router.js
@@ -3,5 +3,6 @@ const express = require('express');
 const route = express.Router({ mergeParams: true });
 
 route.post('/survey', require('./controller/create'));
+route.get('/nps-calculator', require('./controller/list'));
 
 module.exports = route;
