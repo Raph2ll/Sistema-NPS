7c25a62efa4bb9b7e820f09b399ae526d51f66cc
raph2ll <raphael.ph2ll@gmail.com>
2022-11-07 09:05:31 -0300

    feat(api): add router

--- src/api/app.js
+++ src/api/app.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const cors = require('cors');
+const router = require('../router');
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use('/', ((req, res) => {
-  res.send('Test');
-}));
+app.use('/', router);
 
 module.exports = app;
