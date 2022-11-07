24b2d646b2513841a21eed7848cac5fbe41b52f9
raph2ll <raphael.ph2ll@gmail.com>
2022-11-07 15:15:04 -0300

    feat(app): config app to use swagger

--- backend/src/api/app.js
+++ backend/src/api/app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const cors = require('cors');
+const swaggerUi = require('swagger-ui-express');
 const router = require('../router');
+const swaggerDocument = require('../../swagger.json');
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/', router);
 
 module.exports = app;
