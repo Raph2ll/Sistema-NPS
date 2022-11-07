939964e72ec3a739407ff471047914ee1447761b
raph2ll <raphael.ph2ll@gmail.com>
2022-11-07 09:59:00 -0300

    fix(controller): change status-codes

--- src/controller/create.js
+++ src/controller/create.js
@@ -1,4 +1,4 @@
-const statusCodes = require('http-status-codes').StatusCodes;
+const { BAD_REQUEST, CREATED } = require('http-status-codes').StatusCodes;
 const service = require('../service/create');
 const schema = require('../schemas/schemaSurvey');
 
@@ -12,14 +12,14 @@ module.exports = async (req, res) => {
       name, email, value,
     });
 
-    if (error) return res.status(statusCodes.BAD_REQUEST).json(error.message);
+    if (error) return res.status(BAD_REQUEST).json(error.message);
 
     const newSurvey = await service({
       name, email, value,
     });
 
-    return res.status(statusCodes.CREATED).json(newSurvey);
+    return res.status(CREATED).json(newSurvey);
   } catch (err) {
-    return res.status(statusCodes.BAD_REQUEST).json(err);
+    return res.status(BAD_REQUEST).json(err);
   }
 };
