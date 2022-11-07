f1308bcc44652d8df53dddff36e709f51d9e099c
raph2ll <raphael.ph2ll@gmail.com>
2022-11-07 10:59:33 -0300

    feat(controller): list controller

---
+++ backend/src/controller/list.js
@@ -0,0 +1,11 @@
+const { OK } = require('http-status-codes').StatusCodes;
+const service = require('../service/list');
+
+module.exports = async (req, res, next) => {
+  try {
+    const result = await service();
+    return res.status(OK).json(result);
+  } catch (err) {
+    return next(err);
+  }
+};
