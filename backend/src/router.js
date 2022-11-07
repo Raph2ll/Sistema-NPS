904f11238b9787246d8adb01957ff008670ec8f4
raph2ll <raphael.ph2ll@gmail.com>
2022-11-07 11:51:42 -0300

    fix(router): path for answers

--- backend/src/router.js
+++ backend/src/router.js
@@ -3,6 +3,6 @@ const express = require('express');
 const route = express.Router({ mergeParams: true });
 
 route.post('/survey', require('./controller/create'));
-route.get('/nps-calculator', require('./controller/list'));
+route.get('/nps-calculator/answers', require('./controller/list'));
 
 module.exports = route;
